b602512ab48dce913c5adf1b5eb2fd5f1cc8b590
kenerg2000 <ken9656@students.fhv.at>
2024-11-22 15:03:51 +0100

    added simple input field, with auto inputfield selection

--- Assets/Scripts/UI_Input.cs
+++ Assets/Scripts/UI_Input.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Input : MonoBehaviour
 {
-    private TMP_InputField inputField;
-    private TextMeshProUGUI output;
+    private string input;
 
-    public void UserInput() {
-        inputField.text = output.text;
+    void Start() {
+        TMP_InputField inputText = GameObject.FindGameObjectWithTag("Input").GetComponent<TMP_InputField>();
+        inputText.ActivateInputField();
+    }
+
+
+    public void UserInput(string s) {
+        input = s;
+        Debug.Log(input);
     }
 }
