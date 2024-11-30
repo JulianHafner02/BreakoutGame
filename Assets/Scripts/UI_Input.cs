d83bbe3f394c937d4574e240648928f1c1e9e7d6
kenerg2000 <ken9656@students.fhv.at>
2024-11-30 14:47:21 +0100

    writing maths eq in file

--- Assets/Scripts/UI_Input.cs
+++ Assets/Scripts/UI_Input.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using TMPro;
@@ -17,7 +18,11 @@ public class UI_Input : MonoBehaviour
     private GameController gameController;
     private int mathresult;
 
+    private int index = -1;
+    private string filePath = "Assets/equations.txt";
 
+
+    private Equation[] equations = new Equation[10];
 
     void Start() {
         inputText = GameObject.FindGameObjectWithTag("Input").GetComponent<TMP_InputField>();
@@ -27,19 +32,23 @@ public class UI_Input : MonoBehaviour
     
     public void UserInput() {
         inputResult = Convert.ToInt32(inputText.text);
-        Debug.Log(inputResult);
+        equations[index].answer = inputResult;
         inputText.text = "";
         inputText.ActivateInputField();
-        Debug.Log("InputResult" + inputResult + "MathResult" + mathresult);
         if (inputResult == mathresult) {
             mathq.text = "Correct";
             mathq.color = Color.green;
             gameController.AddScore(500);
+            equations[index].isCorrect = true;
+            WriteArrayToFile(equations[index], filePath);
         } else {
             mathq.text = "Incorrect";
             mathq.color = Color.red;
             gameController.AddScore(-250);
+            equations[index].isCorrect = false;
+            WriteArrayToFile(equations[index], filePath);
         }
+        Debug.Log(equations[index].equationString +" "+ equations[index].result +" "+ equations[index].answer + " " + equations[index].isCorrect); 
     }
 
     public int GetInput() {
@@ -52,6 +61,34 @@ public class UI_Input : MonoBehaviour
         return number;
     }
 
+    public struct Equation {
+        public string equationString;
+        public int result;
+        public int answer;
+        public Boolean isCorrect;
+
+        public Equation(string equationString, int result, int answer, Boolean isCorrect) {
+
+            this.equationString = equationString;
+            this.result = result;
+            this.answer = answer;
+            this.isCorrect = isCorrect;
+        }
+
+        public override string ToString()
+        {
+            return $"{equationString},{result},{answer},{isCorrect}";
+        }
+    }
+
+    public static void WriteArrayToFile(Equation equation, string filePath)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath, append: true))
+        {
+            writer.WriteLine(equation.ToString());
+        }
+    }
+
     public void InvokeMathEvent() {
         TextMeshProUGUI mathq = GameObject.Find("Math").GetComponent<TextMeshProUGUI>();
         mathq.color = Color.black;
@@ -59,6 +96,7 @@ public class UI_Input : MonoBehaviour
         int number2 = GenerateRandomNumber();
         mathq.text = number1 + " + " + number2 + " =";
         mathresult = number1 + number2;
-        Debug.Log("Brickscript:" + mathresult);
+        index += 1;
+        equations[index] = new Equation(number1 + " + " + number2, mathresult, 0, false);
     }
 }
